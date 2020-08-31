9e9a4db822a959c8da1fde05c6bc4c5e39f3bcfa
Aindrila Bhattacharjee <47319230+aindrila22@users.noreply.github.com>
2020-08-31 13:45:08 +0530

    Delete keys.js

--- keys.js
+++
@@ -1,4 +0,0 @@
-module.exports={
-    MONGOURI:"mongodb+srv://aindrila:No4r8Zvf9DaMTeun@cluster0.xdi4z.mongodb.net/<dbname>?retryWrites=true&w=majority",
-    JWT_SECRET:"serfgtuomvhdndhejdkm1458"
-}
